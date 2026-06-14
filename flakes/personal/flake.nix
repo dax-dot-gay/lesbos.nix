b01e2f5fe4d4254a95ed243ef4b8417eaad7c602
Dax Harris <git@dax.gay>
2026-06-14 17:23:50 -0400

    add personal flake

---
+++ flakes/personal/flake.nix
@@ -0,0 +1,43 @@
+{
+    description = "Flake containing configs for my personal systems";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+        sops-nix = {
+            url = "github:Mic92/sops-nix";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        comin = {
+            url = "github:nlewo/comin";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        disko = {
+            url = "github:nix-community/disko/latest";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        nixos-utilities = {
+            url = "github:dax-dot-gay/nixos-utilities";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.sops-nix.follows = "sops-nix";
+            inputs.comin.follows = "comin";
+        };
+        lesbos-common = {
+            url = "path:../common";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.sops-nix.follows = "sops-nix";
+            inputs.comin.follows = "comin";
+            inputs.nixos-utilities.follows = "nixos-utilities";
+        };
+    };
+
+    outputs =
+        { self, ... }@inputs:
+        let
+            system = "x86_64-linux";
+            pkgs = import ../../util/pkgsconf.nix {
+                inherit inputs system;
+                extraOverlays = [ ];
+            };
+        in
+        {
+        };
+}
