ba6cecd0052401c4181aaee0d0c5fe4a624cbb9d
Dax Harris <git@dax.gay>
2026-06-16 17:52:06 -0400

    add netconf

--- flakes/homelab/hosts/router/default.nix
+++ flakes/homelab/hosts/router/default.nix
@@ -6,4 +6,17 @@
     ...
 }:
 {
+    lesbos.proxmox = {
+        enable = true;
+        network = {
+            primary = {
+                bridge = "vmbr0";
+            };
+            extra_interfaces = [
+                {
+                    bridge = "vmbr1";
+                }
+            ];
+        };
+    };
 }
