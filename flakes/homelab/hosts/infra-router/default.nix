9dace403b17714aea818e76a2d467f4f615e5c82
Dax Harris <git@dax.gay>
2026-06-16 23:20:44 -0400

    configure router

--- flakes/homelab/hosts/infra-router/default.nix
+++ flakes/homelab/hosts/infra-router/default.nix
@@ -10,6 +10,25 @@
     lesbos = {
         proxmox = {
             enable = true;
+            network = {
+                primary.bridge = "vmbr0";
+                extra_interfaces = [
+                    {
+                        bridge = "vmbr3";
+                    }
+                ];
+            };
+            watchdog.enable = true;
+            start = {
+                on_boot = true;
+                on_deploy = true;
+                order = 1;
+                delay_up = 10;
+                delay_down = 10;
+            };
+            resources.cores = 4;
+            resources.memory = 4096;
+            storage.disk_size = 65536; # 64 GiB
         };
         base.users = {
             root = {
