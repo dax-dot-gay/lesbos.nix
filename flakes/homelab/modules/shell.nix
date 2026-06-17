6ad4ee04651a189775ad23227b1097347a836fa1
Dax Harris <git@dax.gay>
2026-06-17 15:43:12 -0400

    rm fetch

--- flakes/homelab/modules/shell.nix
+++ flakes/homelab/modules/shell.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 {
     programs.zsh = {
         ohMyZsh = {
             enable = true;
             theme = "candy";
         };
-        interactiveShellInit = ''
-            afetch
-        '';
     };
-    environment.systemPackages = [pkgs.afetch];
 }
