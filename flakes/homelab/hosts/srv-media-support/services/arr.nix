638d8f5772feeeec33440d86abb5815c088e0ab4
Dax Harris <git@dax.gay>
2026-06-21 16:48:07 -0400

    setup arr

---
+++ flakes/homelab/hosts/srv-media-support/services/arr.nix
@@ -0,0 +1,72 @@
+{ config, ... }:
+let
+    mkArr = name: port: {
+        enable = true;
+        dataDir = "/media-support/services/arrs/${name}";
+        environmentFiles = [
+            config.sops.templates."${name}.env".path
+        ];
+        user = name;
+        group = name;
+        openFirewall = true;
+        settings = {
+            log.analyticsEnabled = false;
+            app.instancename = "lesbos-${name}";
+            auth = {
+                enabled = true;
+                required = true;
+                method = "Forms";
+            };
+            update.mechanism = "external";
+            server = {
+                port = port;
+                bindaddress = "0.0.0.0";
+            };
+        };
+    };
+in
+{
+    lesbos.secrets.system = {
+        "arr/sonarr.key" = {
+            owner = "sonarr";
+            group = "sonarr";
+            mode = "0400";
+        };
+        "arr/radarr.key" = {
+            owner = "radarr";
+            group = "radarr";
+            mode = "0400";
+        };
+        "arr/prowlarr.key" = {
+            owner = "prowlarr";
+            group = "prowlarr";
+            mode = "0400";
+        };
+    };
+    sops.templates = {
+        "sonarr.env" = {
+            owner = "sonarr";
+            group = "sonarr";
+            mode = "0400";
+            content = "SONARR__AUTH__APIKEY=${config.sops.placeholder."arr/sonarr.key"}";
+        };
+        "radarr.env" = {
+            owner = "radarr";
+            group = "radarr";
+            mode = "0400";
+            content = "RADARR__AUTH__APIKEY=${config.sops.placeholder."arr/radarr.key"}";
+        };
+        "prowlarr.env" = {
+            owner = "prowlarr";
+            group = "prowlarr";
+            mode = "0400";
+            content = "PROWLARR__AUTH__APIKEY=${config.sops.placeholder."arr/prowlarr.key"}";
+        };
+    };
+
+    services = {
+        sonarr = mkArr "sonarr" 8989;
+        radarr = mkArr "radarr" 7878;
+        prowlarr = mkArr "prowlarr" 9696;
+    };
+}
--- flakes/homelab/hosts/srv-media-support/services/default.nix
+++ flakes/homelab/hosts/srv-media-support/services/default.nix
@@ -2,5 +2,6 @@
 {
     imports = [
         ./deluge.nix
+        ./arr.nix
     ];
 }
