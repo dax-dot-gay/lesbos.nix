db33c785c4cabc5d40595cbd6d2ff21c1d6a8d67
Dax Harris <git@dax.gay>
2026-06-28 15:29:02 -0400

    dont set shelfarr key

--- flakes/homelab/hosts/srv-media-support/services/shelfarr.nix
+++ flakes/homelab/hosts/srv-media-support/services/shelfarr.nix
@@ -1,19 +1,5 @@
 { config, ... }:
 {
-    lesbos.secrets.system."shelfarr/key" = {
-        owner = "shelfarr";
-        group = "shelfarr";
-        mode = "0400";
-    };
-    sops.templates."shelfarr.env" = {
-        owner = "shelfarr";
-        group = "shelfarr";
-        mode = "0400";
-        content = ''
-            RAILS_MASTER_KEY=${config.sops.placeholder."shelfarr/key"}
-        '';
-    };
-
     virtualisation.oci-containers = {
         backend = "podman";
         containers.shelfarr = {
@@ -28,7 +14,6 @@
                 CHOWN_ON_START = "never";
                 SOLID_QUEUE_IN_PUMA = "1";
             };
-            environmentFiles = [ config.sops.templates."shelfarr.env".path ];
             volumes = [
                 "/media-support/services/shelfarr/data:/rails/storage"
                 "/media-support/media/Audiobooks:/audiobooks"
