961477d32f7f396f15be73862c7b1afb7e7a8807
Dax Harris <git@dax.gay>
2026-07-12 13:41:34 -0400

    add woodpecker conf

--- flakes/homelab/hosts/sys-cicd/default.nix
+++ flakes/homelab/hosts/sys-cicd/default.nix
@@ -6,7 +6,10 @@
     ...
 }:
 {
-    imports = [ ./provision-secrets.nix ];
+    imports = [
+        ./provision-secrets.nix
+        ./woodpecker.nix
+    ];
     lesbos = {
         info = {
             canonicalName = "sys-cicd";
@@ -16,6 +19,24 @@
         };
         proxmox = {
             enable = true;
+            storage = {
+                disk_size = "128G";
+                virtiofs = [
+                    {
+                        name = "data";
+                        mount = true;
+                        id = "DATA";
+                        expose_acl = true;
+                        expose_xattr = true;
+                    }
+                ];
+            };
+            resources = {
+                cores = 6;
+                memory = 16384;
+            };
+            start.order = 100;
+            network.primary.bridge = "vmbr3";
         };
         base.users = {
             root = {
@@ -28,5 +49,32 @@
             };
             users = { };
         };
+        volumes = {
+            woodpecker = {
+                enable = true;
+                source = {
+                    type = "share";
+                    name = "data";
+                    path = "/systems/sys-cicd/woodpecker";
+                    ensureSource.enable = true;
+                };
+                destination = "/var/lib/woodpecker";
+                strategy.bindMapped = {
+                    enable = true;
+                    permissions = "0700";
+                    user = "woodpecker";
+                    group = "woodpecker";
+                };
+                required_by = ["woodpecker-server.service" "woodpecker-agent-docker.service"];
+            };
+        };
+    };
+
+    users = {
+        users.woodpecker = {
+            isSystemUser = false;
+            group = "woodpecker";
+        };
+        groups.woodpecker = {};
     };
 }
---
+++ flakes/homelab/hosts/sys-cicd/woodpecker.nix
@@ -0,0 +1,53 @@
+{ config, ... }:
+{
+    lesbos.secrets.system = {
+        "woodpecker.env" = {
+            owner = "woodpecker";
+            group = "woodpecker";
+            mode = "0400";
+        };
+    };
+    services.woodpecker-server = {
+        enable = true;
+        environment = {
+            WOODPECKER_HOST = "https://woodpecker.dax.gay";
+            WOODPECKER_OPEN = "false";
+            WOODPECKER_ADMIN = "dax-dot-gay";
+            WOODPECKER_SERVER_ADDR = "0.0.0.0:3007";
+            WOODPECKER_GRPC_ADDR = ":9000";
+        };
+        environmentFile = [ config.sops.secrets."woodpecker.env".path ];
+    };
+    services.woodpecker-agents.agents."docker" = {
+        enable = true;
+        extraGroups = [ "podman" ];
+        environment = {
+            WOODPECKER_SERVER = "localhost:9000";
+            WOODPECKER_BACKEND = "docker";
+            DOCKER_HOST = "unix:///run/podman/podman.sock";
+            WOODPECKER_MAX_WORKFLOWS = "4";
+        };
+        environmentFile = [ config.sops.secrets."woodpecker.env".path ];
+    };
+    virtualisation.podman = {
+        enable = true;
+        defaultNetwork.settings = {
+            dns_enabled = true;
+        };
+    };
+    networking.firewall.interfaces."podman0" = {
+        allowedUDPPorts = [ 53 ];
+        allowedTCPPorts = [ 53 ];
+    };
+    networking.firewall.allowedTCPPorts = [ 3007 ];
+    systemd.services = {
+        woodpecker-server.serviceConfig = {
+            User = "woodpecker";
+            Group = "woodpecker";
+        };
+        woodpecker-agent-docker.serviceConfig = {
+            User = "woodpecker";
+            Group = "woodpecker";
+        };
+    };
+}
