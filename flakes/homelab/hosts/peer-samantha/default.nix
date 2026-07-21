a977aa9a652deddf06a2c61561a7c2241ddb85d3
Dax Harris <git@dax.gay>
2026-07-21 17:39:43 -0400

    ssh key again

--- flakes/homelab/hosts/peer-samantha/default.nix
+++ flakes/homelab/hosts/peer-samantha/default.nix
@@ -49,7 +49,11 @@
                     enable = true;
                     ssh = {
                         enable = true;
-                        authorizedKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtv697nzcNal/a/n87sy8rSqnFxRi9N0U61kKHyEc1z" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAdqW9W+ybE5OQkhUxllToVBMuGfurb5yQNxFaeoukd sambamfa@tundra"];
+                        authorizedKeys = [
+                            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtv697nzcNal/a/n87sy8rSqnFxRi9N0U61kKHyEc1z"
+                            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAdqW9W+ybE5OQkhUxllToVBMuGfurb5yQNxFaeoukd sambamfa@tundra"
+                            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC31Cl7SOYltE8vdbHs3XFFMvWc7RKoO9lhGZ7qS1OtO+pJ+u3dvzxoX0cpcd6GFA9tXilF/S/iRMa+CcXLaaGKelrSIPhcbne5od788KoEo6vHyhjs2KTSkIh12R2rj2LspPVwYwhsxCLCs6wsqFQk6a993ZUuU0D6py/75F1qatRAetsKXHhM4kxNUqou0Rrl9yN7LKwklRbLdxVeKv5dmMAEYpHHHACWVbCTqYN1fXDnQqfvHa/f56TGhfvsC8SK6AIlf0MN3dRqI6kleF2RGq0R+wSN231kxkS8YkACF/KyahU3mMeEvJKZ7R19vcsQqK79nscSlKb4eOZc5CmzPZHqBEDuemqshu1KEOBThG0eRT14ygIyk+DahXXUUJc1zX+O0FGCq0kmMygF5MvahmIW3qcCr/O/446iq9sEhoNhEfFUHAuURtV1SHxA+5jk3c4hhnlw9Qoqd6o28KvfkRlNO/jZy6znOjV49akbdpkWVJdiTRj2eEXeOSYOcuhiZAmAZlD5xx7gZtt+f84FqeDxCJ1Rp2L6AzZDJdQXI28txLUB6r80wWZ"
+                        ];
                     };
                     password = {
                         enable = true;
@@ -81,7 +85,10 @@
     };
     services.tailscale = {
         enable = true;
-        extraSetFlags = [ "--advertise-exit-node" "--advertise-routes=192.168.64.0/24,192.168.0.0/24" ];
+        extraSetFlags = [
+            "--advertise-exit-node"
+            "--advertise-routes=192.168.64.0/24,192.168.0.0/24"
+        ];
         useRoutingFeatures = "server";
     };
     networking.nftables.enable = true;
