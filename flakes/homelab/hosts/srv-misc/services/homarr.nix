1d8636f7de398027f359c7e9bfd3186dea05629c
Dax Harris <git@dax.gay>
2026-07-27 17:03:28 -0400

    setup homarr oidc

--- flakes/homelab/hosts/srv-misc/services/homarr.nix
+++ flakes/homelab/hosts/srv-misc/services/homarr.nix
@@ -1,12 +1,18 @@
 { config, ... }:
 {
-    lesbos.secrets.system."homarr/key" = { };
+    lesbos.secrets.system = {
+        "homarr/key" = { };
+        "homarr/oidc/client_id" = { };
+        "homarr/oidc/client_secret" = { };
+    };
     sops.templates."homarr.env" = {
         owner = "root";
         group = "root";
         mode = "0400";
         content = ''
             SECRET_ENCRYPTION_KEY=${config.sops.placeholder."homarr/key"}
+            AUTH_OIDC_CLIENT_ID=${config.sops.placeholder."homarr/oidc/client_id"}
+            AUTH_OIDC_CLIENT_SECRET=${config.sops.placeholder."homarr/oidc/client_secret"}
         '';
     };
     virtualisation.oci-containers.containers.homarr = {
@@ -19,6 +25,19 @@
             "0.0.0.0:7575:7575"
         ];
         environmentFiles = [ config.sops.templates."homarr.env".path ];
+        environment = {
+            BASE_URL = "https://home.dax.gay";
+            NEXTAUTH_URL = "https://home.dax.gay";
+            AUTH_PROVIDERS = "oidc";
+            AUTH_OIDC_ISSUER = "https://auth.dax.gay/application/o/homarr/";
+            AUTH_OIDC_URI = "https://auth.dax.gay/application/o/authorize/";
+            AUTH_OIDC_CLIENT_NAME = "Lesbos SSO";
+            AUTH_OIDC_SCOPE_OVERWRITE = "openid email profile groups";
+            AUTH_OIDC_GROUPS_ATTRIBUTE = "groups";
+            AUTH_LOGOUT_REDIRECT_URL = "https://auth.dax.gay/application/o/homarr/end-session/";
+            AUTH_OIDC_AUTO_LOGIN = "true";
+            ADMIN_GROUP = "app:homarr:admin";
+        };
         autoStart = true;
     };
     networking.firewall.allowedTCPPorts = [ 7575 ];
