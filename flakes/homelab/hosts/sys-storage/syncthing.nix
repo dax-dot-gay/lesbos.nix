f570ffb086b17a63df5d1d6043e9f8b7ef69b069
Dax Harris <git@dax.gay>
2026-09-09 19:40:57 -0400

    remove syncthing config

--- flakes/homelab/hosts/sys-storage/syncthing.nix
+++ flakes/homelab/hosts/sys-storage/syncthing.nix
@@ -8,19 +8,8 @@
     services.syncthing = {
         enable = true;
         guiAddress = "0.0.0.0:8384";
-        guiPasswordFile = config.sops.secrets."syncthing/password".path;
-        settings = {
-            gui.user = "dax";
-            folders = {
-                secrets = {
-                    path = "/syncthing/shared-folders/secrets";
-                };
-                misc = {
-                    path = "/syncthing/shared-folders/misc";
-                };
-            };
-        };
         dataDir = "/syncthing/service";
+        
     };
     networking.firewall.allowedTCPPorts = [
         22000
